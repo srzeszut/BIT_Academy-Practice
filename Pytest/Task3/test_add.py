f3d5314f037456fafe0de977cae811ecbc663c77
Szczepan Rzeszutek <szcz113@gmail.com>
2022-07-26 23:57:05 +0200

    adding complex commit task3

--- Pytest/Task3/test_add.py
+++ Pytest/Task3/test_add.py
@@ -1,4 +1,13 @@
 from cmplex import C
+import pytest
 
 # Napisać testy sprawdzające dodawanie liczb zespolonych
 # w załączonym pliku
+results=[(1,2,3,4,4,6),
+         (1,2,3,4,4,6),
+         (1,2,3,4,4,6),
+         (1,2,3,4,4,6),
+         (1,2,3,4,4,6),]
+@pytest.mark.parametrize('re_x,im_x,re_y,im_y,re_res,im_res',results)
+def test_add_complex(re_x,im_x,re_y,im_y,re_res,im_res):
+    assert C(re_x,im_x)+C(re_y,im_y)==C(re_res,im_res)
