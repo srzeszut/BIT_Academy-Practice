aca9925deca5d06f91558dd112fe0eccde814a76
Szczepan Rzeszutek <szcz113@gmail.com>
2022-07-27 10:37:06 +0200

    second task commit

--- Pytest/Task2/test_class.py
+++ Pytest/Task2/test_class.py
@@ -1,5 +1,6 @@
 from function import is_prime
 from random import randint
+from solution import solution
 import pytest
 
 class TestMini:
@@ -23,11 +24,11 @@ class TestMini:
 class TestMid:
     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
     # liczb z przedziału <10 ** 4, 10 ** 5>
-    primes_mid = [(-1, True),
-              (0, False),
-              (1, False),
-              (2, True),
-              (3, True)]
+    primes_mid=[]
+    for i in range(5):
+        x=randint(10 ** 4, 10 ** 5)
+        primes_mid.append((x,solution(x)))
+
 
     @pytest.mark.parametrize('number,prime', primes_mid)
     def test_mini_primes(self, number, prime):
@@ -39,11 +40,11 @@ class TestMaxi:
     # z przedziału <10 ** 12, 10 ** 13>, a następnie
     # naprawić funkcję is_prime aby testy wykonywały się szybciej
     # niż dla aktualnej funkcji
-    primes_maxi = [(-1, True),
-                  (0, False),
-                  (1, False),
-                  (2, True),
-                  (3, True)]
+    primes_maxi = []
+    for i in range(5):
+        x = randint(10 ** 12, 10 ** 13)
+        primes_maxi.append((x, solution(x)))
+        #primes_maxi.append((87178291199,solution(87178291199)))
 
     @pytest.mark.parametrize('number,prime', primes_maxi)
     def test_mini_primes(self, number, prime):
