b479d53904c0c96cd1a8b5fc0fece08ab80cbbe1
Szymon Rusiecki <79122265+Rusiek@users.noreply.github.com>
2022-08-06 14:18:18 +0200

    Delete OOP/Task2/Task2 directory

--- OOP/Task2/Task2/SmartHouse/main.py
+++
@@ -1,8 +0,0 @@
-# Drzwi, okna -> otwórz, zamknij
-# Światło, -> włącz, wyłącz, natężenie
-# Światło kolorowe -> włącz, wyłącz, natężenie, kolor(RGB)
-# Czajnik, TV, Kuchenka, Pralka, Zmywarka -> włącz, wyłącz
-
-class SmartHouse:
-    def __init__() -> None:
-        pass
